5f2bbe1ee196954f754fb589df8f431a07a7c929
redpanda3 <yuda@berkeley.edu>
2019-12-11 06:13:50 -0800

    .

---
+++ soDLA/src/main/scala/slibs/MUX2HDD2.scala
@@ -0,0 +1,17 @@
+package nvdla
+
+import chisel3._
+import chisel3.experimental._
+
+@chiselName
+class MUX2HDD2 extends Module {
+  val io = IO(new Bundle {
+    val I0 = Input(Bool())
+    val I1 = Input(Bool())
+    val S = Input(Bool())
+    val Z = Output(Bool())
+  })
+  io.Z := Mux(io.S, io.I1, io.I0)
+}
+
+
---
+++ soDLA/src/main/scala/slibs/PGAOPV_INVD2PO4.scala
@@ -0,0 +1,13 @@
+package nvdla
+
+import chisel3._
+import chisel3.experimental._
+
+@chiselName
+class PGAOPV_INVD2PO4 extends Module {
+  val io = IO(new Bundle {
+    val I = Input(Bool())
+    val ZN = Output(Bool())
+  })
+  io.ZN := ~io.I 
+}
